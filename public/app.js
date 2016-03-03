d286ad2f3021592a70246220772dd154bd355cd4
Zach Baker <zach.r.baker1@gmail.com>
2016-03-02 17:32:10 -0800

    services

--- public/app.js
+++ public/app.js
@@ -12,6 +12,6 @@ angular.module('app', ['ui.router'])
 
 
 		$urlRouterProvider
-			.otherwise('/login');
+			.otherwise('/main');
 	})
 
--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -3,6 +3,6 @@ angular.module('app')
 	.controller('mainCtrl', ['$scope', '$stateParams', '$state',
 	 function($scope, $stateParams, $state){
 		
-		
+		$scope.dude = "hey im a dude."
 		
 	}])
