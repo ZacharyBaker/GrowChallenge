2c9d70950d1131862cd2859b6028c88c311b3049
Zach Baker <zach.r.baker1@gmail.com>
2016-03-02 23:38:24 -0800

    infoBox changes on click

--- public/controllers/repCtrl.js
+++ public/controllers/repCtrl.js
@@ -1,20 +1,113 @@
 angular.module('app')
 
-	.controller('repCtrl', ['$scope', '$state', '$stateParams','mainService',
+	.controller('repCtrl', ['$scope', '$state', '$stateParams', 'mainService',
 		function ($scope, $state, $stateParams, mainService) {
 
-			$scope.getRepresentatives = function () {
-				console.log('stateparams.state', $stateParams.state);
-           
-		   mainService.getRepresentatives($stateParams.state).then(function(response){
-			console.log('THIS IS MAIN CONTROLLER', response);
+			$scope.infoBox = {
+				first: "First Name",
+				last: "Last Name",
+				district: "District",
+				phone: "Phone",
+				office: "Office"
+			}
 			
-			 $scope.representatives = response;  
-		   })
-		   
-       }
-       
-       $scope.getRepresentatives();
+			
+			$scope.getRepresentatives = function () {
 
+				mainService.getRepresentatives($stateParams.state).then(function (response) {
+
+					$scope.representatives = response;
+				})
+
+			}
+
+			$scope.getRepresentatives();
+
+
+
+
+
+			$scope.states = {
+				"AL": "Alabama",
+				"AK": "Alaska",
+				"AZ": "Arizona",
+				"AR": "Arkansas",
+				"CA": "California",
+				"CO": "Colorado",
+				"CT": "Connecticut",
+				"DE": "Delaware",
+				"DC": "District Of Columbia",
+				"FL": "Florida",
+				"GA": "Georgia",
+				"HI": "Hawaii",
+				"ID": "Idaho",
+				"IL": "Illinois",
+				"IN": "Indiana",
+				"IA": "Iowa",
+				"KS": "Kansas",
+				"KY": "Kentucky",
+				"LA": "Louisiana",
+				"ME": "Maine",
+				"MD": "Maryland",
+				"MA": "Massachusetts",
+				"MI": "Michigan",
+				"MN": "Minnesota",
+				"MS": "Mississippi",
+				"MO": "Missouri",
+				"MT": "Montana",
+				"NE": "Nebraska",
+				"NV": "Nevada",
+				"NH": "New Hampshire",
+				"NJ": "New Jersey",
+				"NM": "New Mexico",
+				"NY": "New York",
+				"NC": "North Carolina",
+				"ND": "North Dakota",
+				"OH": "Ohio",
+				"OK": "Oklahoma",
+				"OR": "Oregon",
+				"PW": "Palau",
+				"PA": "Pennsylvania",
+				"RI": "Rhode Island",
+				"SC": "South Carolina",
+				"SD": "South Dakota",
+				"TN": "Tennessee",
+				"TX": "Texas",
+				"UT": "Utah",
+				"VT": "Vermont",
+				"VA": "Virginia",
+				"WA": "Washington",
+				"WV": "West Virginia",
+				"WI": "Wisconsin",
+				"WY": "Wyoming"
+			}
+			
+			$scope.findValue = function(){
+
+				for (var state in $scope.states){
+					
+					
+					if (state === $stateParams.state){
+						
+						$scope.stateVal = $scope.states[state];
+						$scope.key = state;
+					}
+				}
+			}
+			$scope.findValue();
+			
+			$scope.changeInfo = function (rep) {
+				var senName = rep.name.split(' ');
+				rep.first = senName[0];
+				rep.last = senName[1];
+
+				$scope.infoBox = {
+					first: rep.first,
+					last: rep.last,
+					district: rep.district,
+					phone: rep.phone,
+					office: rep.office
+				}
+			}
 
 		}])
--- public/controllers/senatorCtrl.js
+++ public/controllers/senatorCtrl.js
@@ -3,14 +3,18 @@ angular.module('app')
 	.controller('senatorCtrl', ['$scope', '$state', '$stateParams', 'mainService',
 		function ($scope, $state, $stateParams, mainService) {
 
-
-			console.log('stateparams.state', $stateParams.state);
+			$scope.infoBox = {
+				first: "First Name",
+				last: "Last Name",
+				district: "District",
+				phone: "Phone",
+				office: "Office"
+			}
 
 
 			$scope.getSenators = function () {
 
 				mainService.getSenators($stateParams.state).then(function (response) {
-					console.log('THIS IS MAIN CONTROLLER', response);
 
 					$scope.senators = response;
 				})
@@ -18,6 +22,93 @@ angular.module('app')
 			}
 
 			$scope.getSenators();
+
+
+
+
+			$scope.states = {
+				"AL": "Alabama",
+				"AK": "Alaska",
+				"AZ": "Arizona",
+				"AR": "Arkansas",
+				"CA": "California",
+				"CO": "Colorado",
+				"CT": "Connecticut",
+				"DE": "Delaware",
+				"DC": "District Of Columbia",
+				"FL": "Florida",
+				"GA": "Georgia",
+				"HI": "Hawaii",
+				"ID": "Idaho",
+				"IL": "Illinois",
+				"IN": "Indiana",
+				"IA": "Iowa",
+				"KS": "Kansas",
+				"KY": "Kentucky",
+				"LA": "Louisiana",
+				"ME": "Maine",
+				"MD": "Maryland",
+				"MA": "Massachusetts",
+				"MI": "Michigan",
+				"MN": "Minnesota",
+				"MS": "Mississippi",
+				"MO": "Missouri",
+				"MT": "Montana",
+				"NE": "Nebraska",
+				"NV": "Nevada",
+				"NH": "New Hampshire",
+				"NJ": "New Jersey",
+				"NM": "New Mexico",
+				"NY": "New York",
+				"NC": "North Carolina",
+				"ND": "North Dakota",
+				"OH": "Ohio",
+				"OK": "Oklahoma",
+				"OR": "Oregon",
+				"PW": "Palau",
+				"PA": "Pennsylvania",
+				"RI": "Rhode Island",
+				"SC": "South Carolina",
+				"SD": "South Dakota",
+				"TN": "Tennessee",
+				"TX": "Texas",
+				"UT": "Utah",
+				"VT": "Vermont",
+				"VA": "Virginia",
+				"WA": "Washington",
+				"WV": "West Virginia",
+				"WI": "Wisconsin",
+				"WY": "Wyoming"
+			}
+
+			$scope.findValue = function () {
+
+				for (var state in $scope.states) {
+
+
+
+					if (state === $stateParams.state) {
+
+						$scope.stateVal = $scope.states[state];
+						$scope.key = state;
+					}
+				}
+			}
+			$scope.findValue();
+
+			$scope.changeInfo = function (senator) {
+				var senName = senator.name.split(' ');
+				senator.first = senName[0];
+				senator.last = senName[1];
+
+				$scope.infoBox = {
+					first: senator.first,
+					last: senator.last,
+					district: senator.district,
+					phone: senator.phone,
+					office: senator.office
+				}
+			}
 
 
 		}])
